84bbd3e31623e3e89f0699c246baa9d5685223ea
JSH-data <super60447@gmail.com>
2022-09-06 10:51:51 +0900

    sdasd

--- pages/index.tsx
+++ pages/index.tsx
@@ -106,7 +106,7 @@ const Home: NextPage = () => {
         <h2 className={styles.title}>Welcome!</h2>
         <br />
 
-        {/* 모달 adsdasdasasdadasdㅁㅁㄴㅇㅁㄴㅇㅁㄴㅇ */}
+        {/* 모달 adsdasdasasdadasdㅁㅁㄴㅇㅁㄴㅇㅁadsasasdasasdㄴㅇ */}
         <ButtonClose onClick={handleOpen}>Modal Open</ButtonClose>
         <Modal isOpen={isOpen} onClose={handleClose} selector="#modal">
           <ModalBody>
